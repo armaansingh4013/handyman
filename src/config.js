d1613e503ce432ba21bf41d1e769e18ee639c3cb
armaansingh4013 <armaansingh4013@gmail.com>
2024-09-15 11:12:14 +0530

    ui changes

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-// export const api_url = "https://handyman-backend-6055.onrender.com";
-export const api_url = "http://localhost:3001";
+export const api_url = "https://handyman-backend-6055.onrender.com";
+// export const api_url = "http://localhost:3001";
